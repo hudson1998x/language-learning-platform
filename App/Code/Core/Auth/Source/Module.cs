6ae72af929a8e8947f77338879e272bfccd8349d
hudson1998x <jhudson98new@gmail.com>
2026-06-20 22:43:03 +0100

    core(Auth) fixed session issues.

--- App/Code/Core/Auth/Source/Module.cs
+++ App/Code/Core/Auth/Source/Module.cs
@@ -1,4 +1,7 @@
 using LLE.Kernel.Contracts;
+using LLE.Sockets.Events;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LLE.Auth;
 
@@ -7,6 +10,17 @@ public class AuthModule : IModuleLoader
     public async Task AppStart()
     {
         FeatureLoader.LoadFeatures();
+        
+        Eventing.Eventing.Of<KestrelHttpEvents>().WebAppBuilder.Concurrent(builder =>
+        {
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession();
+        });
+        
+        Eventing.Eventing.Of<KestrelHttpEvents>().WebApplication.Concurrent(app =>
+        {
+            app.UseSession();
+        });
     }
 
     public Task AppStop() => Noop();
